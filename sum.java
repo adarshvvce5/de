8e1ef291a562fc6f866146fb205b2fd1d7c0e3a1
adarshvvce5 <159601998+adarshvvce5@users.noreply.github.com>
2024-02-10 14:45:08 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum{
+    public static void main(string[]args){
+        int a=10;
+        int b=10;
+        int sum=0;
+        System.out.println("sum"+a+"and"+b+ "is"+sum);
+    }
+}
